3d5129eef58be0fcd97f257dd814ddf710cc1fd0
Dylanrock333 <dylanrock333@gmail.com>
2025-04-09 17:40:45 -0500

    main port change

--- app/main.py
+++ app/main.py
@@ -28,6 +28,7 @@ app.include_router(router, prefix="/api")
 
 # Main execution
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=True)
     
     
